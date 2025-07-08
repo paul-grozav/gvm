85fba240ef3f652a0cb8378acff09a27d67ae6ad
Tancredi-Paul Grozav <paul@grozav.info>
2025-07-08 17:25:30 +0300

    key inputs

--- os/ms-dos.sh
+++ os/ms-dos.sh
@@ -31,16 +31,46 @@ fi &&
 
 # Create virtual disk
 hdd_file="${script_dir}/vm.img" &&
-qemu-img create -f qcow2 ${hdd_file} 4G &&
+if [ ${scratch} -eq 1 ]
+then
+  rm -f ${hdd_file}
+fi &&
+if [ ! -f ${hdd_file} ]
+then
+  qemu-img create -f qcow2 ${hdd_file} 50M
+fi &&
 
-qemu-system-x86_64 \
+qemu_pid="$( qemu-system-x86_64 \
   -m 16M \
   -smp 1 \
+  -monitor unix:"${input_socket}",server,nowait \
   ` # -display none ` \
   ` # -serial stdio ` \
   -hda ${hdd_file} \
   -cdrom ${cdrom_file} \
-  &&
+  1>/dev/null \
+  2>&1 \
+  & 
+  echo ${!} )" &&
+# qemu_pid=${!} &&
+sleep 7 &&
+# Select install and press enter
+echo "sendkey 3" | socat - UNIX-CONNECT:"${input_socket}" &&
+sleep 0.5 &&
+echo "sendkey ret" | socat - UNIX-CONNECT:"${input_socket}" &&
+# Wait for installer to start
+sleep 35 &&
+# Enter to start Setup
+echo "sendkey ret" | socat - UNIX-CONNECT:"${input_socket}" &&
+sleep 1 &&
+# Choose: Configure unallocated disk space (recommended)
+echo "sendkey ret" | socat - UNIX-CONNECT:"${input_socket}" &&
+# Press enter to pick up Setup Disk #1
+echo "sendkey ret" | socat - UNIX-CONNECT:"${input_socket}" &&
+
+# sleep 5 &&
+# kill -SIGTERM ${qemu_pid} &&
+# wait ${qemu_pid} &&
 
 # Stop debugging
 ( [ ${should_debug} -eq 1 ] && set +x || true ) &&
