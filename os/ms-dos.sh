1e1fe64f7053daa78eabc445aedfab74c2e3d552
Tancredi-Paul Grozav <paul@grozav.info>
2025-07-08 17:48:14 +0300

    ms-dos

--- os/ms-dos.sh
+++ os/ms-dos.sh
@@ -25,8 +25,14 @@ fi &&
 
 if [ ! -f ${cdrom_file} ]
 then
-  wget https://archive.org/download/DOS6.22ISO/MS-DOS%206.22.iso \
-    -O ${cdrom_file}
+  url="https://archive.org/download/ms-dos-6.22_dvd/MS-DOS%206.22.iso" &&
+  url="https://archive.org/download/DOS6.22ISO/MS-DOS%206.22.iso" &&
+  url="https://dl-alt1.winworldpc.com/Microsoft%20MS-DOS%206.22%20Plus%20Enhanced%20Tools%20(3.5).7z" &&
+  wget ${url} -O ${cdrom_file} &&
+  7z x ${cdrom_file} &&
+  mv ${script_dir}/Microsoft\ MS-DOS\ 6.22\ Plus\ Enhanced\ Tools\ \(3.5\)/ \
+    ${script_dir}/ms-dos-setup &&
+  true
 fi &&
 
 # Create virtual disk
@@ -41,32 +47,45 @@ then
 fi &&
 
 qemu_pid="$( qemu-system-x86_64 \
+  ` # RAM for the machine ` \
   -m 16M \
+  ` # CPU cores for the machine ` \
   -smp 1 \
+  ` # Using socket file to send commands to qemu console, and key strokes ` \
   -monitor unix:"${input_socket}",server,nowait \
   ` # -display none ` \
   ` # -serial stdio ` \
   -hda ${hdd_file} \
-  -cdrom ${cdrom_file} \
+  ` # Mount ISO as CD-ROM ` \
+  ` # -cdrom ${cdrom_file} ` \
+  ` # Mount Floppy disk ` \
+  -fda ${script_dir}/ms-dos-setup/Disk1.img \
+  ` # Boot from CD-ROM ` \
+  -boot a \
   1>/dev/null \
   2>&1 \
   & 
   echo ${!} )" &&
-# qemu_pid=${!} &&
-sleep 7 &&
-# Select install and press enter
-echo "sendkey 3" | socat - UNIX-CONNECT:"${input_socket}" &&
-sleep 0.5 &&
-echo "sendkey ret" | socat - UNIX-CONNECT:"${input_socket}" &&
-# Wait for installer to start
-sleep 35 &&
-# Enter to start Setup
-echo "sendkey ret" | socat - UNIX-CONNECT:"${input_socket}" &&
-sleep 1 &&
-# Choose: Configure unallocated disk space (recommended)
-echo "sendkey ret" | socat - UNIX-CONNECT:"${input_socket}" &&
-# Press enter to pick up Setup Disk #1
-echo "sendkey ret" | socat - UNIX-CONNECT:"${input_socket}" &&
+
+# url="https://archive.org/download/ms-dos-6.22_dvd/MS-DOS%206.22.iso" &&
+# This is only live - no way to install it
+
+# url="https://archive.org/download/DOS6.22ISO/MS-DOS%206.22.iso" &&
+# Installer can not find floppy disk
+# sleep 7 &&
+# # Select install and press enter
+# echo "sendkey 3" | socat - UNIX-CONNECT:"${input_socket}" &&
+# sleep 0.5 &&
+# echo "sendkey ret" | socat - UNIX-CONNECT:"${input_socket}" &&
+# # Wait for installer to start
+# sleep 35 &&
+# # Enter to start Setup
+# echo "sendkey ret" | socat - UNIX-CONNECT:"${input_socket}" &&
+# sleep 1 &&
+# # Choose: Configure unallocated disk space (recommended)
+# echo "sendkey ret" | socat - UNIX-CONNECT:"${input_socket}" &&
+# # Press enter to pick up Setup Disk #1
+# echo "sendkey ret" | socat - UNIX-CONNECT:"${input_socket}" &&
 
 # sleep 5 &&
 # kill -SIGTERM ${qemu_pid} &&
